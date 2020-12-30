11fa829c3d182e4120888697ef9841a475bc24a5
SaadSalim <shuvrohossain8@gmail.com>
2020-12-30 23:20:04 +0600

    modified

--- MID/src/Compare.java
+++ MID/src/Compare.java
@@ -3,11 +3,11 @@ public class Compare {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s1 = "Md;Ruhul-Amin";
-		int a = s1.indexOf(";");
-		int b = s1.lastIndexOf("-");
-		String s2 = s1.substring(a+1, b);
-		System.out.println(s2);
+		String s5 = "Saad;Ahmed-Salim";
+		int a = s5.indexOf(";");
+		int b = s5.lastIndexOf("-");
+		String s6 = s5.substring(a+1, b);
+		System.out.println(s6);
 		
 
 	}
